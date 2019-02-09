309c71b93867a2f475171def2658868cf3359dd0
dhaker hachicha <dhaker.hachicha@gmail.com>
2019-02-09 13:34:32 +0100

    1st commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ 
+ /**
+  * A greeter method for greeting
+  * */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
